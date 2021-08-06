85541a4605e2330f163d5a74cfe342d399451476
Rafael Mota Alves <rma7@cin.ufpe.br>
2021-08-06 19:15:36 -0300

    Adicionar MLP com metricas

--- trn/utils.py
+++ trn/utils.py
@@ -1,12 +1,77 @@
 import numpy as np
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
+from sklearn.metrics import roc_auc_score, average_precision_score
+
+import scikitplot as skplt
+import matplotlib.pyplot as plt
 
 def load_dataset():
     trainfile = np.load("data/train.npz")
     testfile = np.load("data/test.npz")
-    valfile = np.load("data/val.npz")
 
     x_train, y_train = trainfile["x"], trainfile["y"]
     x_test, y_test = testfile["x"], testfile["y"]
-    x_val, y_val = valfile["x"], valfile["y"]
 
-    return x_train, x_val, x_test, y_train, y_val, y_test
+    return x_train, x_test, y_train, y_test
+
+def extract_final_losses(history):
+    """Função para extrair o melhor loss de treino e validação.
+    
+    Argumento(s):
+    history -- Objeto retornado pela função fit do keras.
+    
+    Retorno:
+    Dicionário contendo o melhor loss de treino e de validação baseado 
+    no menor loss de validação.
+    """
+    train_loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    idx_min_val_loss = np.argmin(val_loss)
+    return {'train_loss': train_loss[idx_min_val_loss], 'val_loss': val_loss[idx_min_val_loss]}
+
+def plot_training_error_curves(history):
+    """Função para plotar as curvas de erro do treinamento da rede neural.
+    
+    Argumento(s):
+    history -- Objeto retornado pela função fit do keras.
+    
+    Retorno:
+    A função gera o gráfico do treino da rede e retorna None.
+    """
+    train_loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    
+    fig, ax = plt.subplots()
+    ax.plot(train_loss, label='Train')
+    ax.plot(val_loss, label='Validation')
+    ax.set(title='Training and Validation Error Curves', xlabel='Epochs', ylabel='Loss (MSE)')
+    ax.legend()
+    plt.show()
+
+def compute_performance_metrics(y, y_pred_class, y_pred_scores=None):
+    y = np.argmax(y, axis=1)
+    y_pred_class = np.argmax(y_pred_class, axis=1)
+    accuracy = accuracy_score(y, y_pred_class)
+    recall = recall_score(y, y_pred_class)
+    precision = precision_score(y, y_pred_class)
+    f1 = f1_score(y, y_pred_class)
+    performance_metrics = (accuracy, recall, precision, f1)
+    if y_pred_scores is not None:
+        skplt.metrics.plot_ks_statistic(y, y_pred_scores)
+        plt.show()
+        y_pred_scores = y_pred_scores[:, 1]
+        auroc = roc_auc_score(y, y_pred_scores)
+        aupr = average_precision_score(y, y_pred_scores)
+        performance_metrics = performance_metrics + (auroc, aupr)
+    return performance_metrics
+
+def print_metrics_summary(accuracy, recall, precision, f1, auroc=None, aupr=None):
+    print()
+    print("{metric:<18}{value:.4f}".format(metric="Accuracy:", value=accuracy))
+    print("{metric:<18}{value:.4f}".format(metric="Recall:", value=recall))
+    print("{metric:<18}{value:.4f}".format(metric="Precision:", value=precision))
+    print("{metric:<18}{value:.4f}".format(metric="F1:", value=f1))
+    if auroc is not None:
+        print("{metric:<18}{value:.4f}".format(metric="AUROC:", value=auroc))
+    if aupr is not None:
+        print("{metric:<18}{value:.4f}".format(metric="AUPR:", value=aupr))
